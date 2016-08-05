ea78fc545b5051577418bf03d8e241bdcad53679
Brad Searle <bobthebutcher@users.noreply.github.com>
2016-08-05 19:43:51 +1000

    added init method

--- eos.py
+++ eos.py
@@ -1,9 +1,20 @@
 from __future__ import print_function
 
 import pyeapi
+from pyeapi.eapilib import CommandError, ConnectionError
 
 from . helpers import Result
 
+
 class EAPI(object):
-    def __init__(self):
-        pass
+    def __init__(self, host, username, password):
+        self.host = host
+        self.username = username
+        self.password = password
+
+        self.conn = pyeapi.connect(host=host, username=username, password=password)
+
+        try:
+            self.conn.execute('enable')
+        except ConnectionError as e:
+            raise e
